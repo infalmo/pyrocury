84e036eb4f793862fe4881a097536ee436c71888
s-AndrewJia <andrewkia3@gmail.com>
2022-08-26 19:11:25 -0400

    more links

--- client/pyrocury.py
+++ client/pyrocury.py
@@ -4,8 +4,32 @@ import tkinter.messagebox
 import customtkinter
 import time
 
-window = customtkinter.CTk()
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
 
+window = customtkinter.CTk()
+window.geometry("800x400")
+window.title("Pyrocury")
+
+# change logo maybe if don't like
+logo = PhotoImage(file="icon.png")
+window.iconphoto(True, logo)
+
+# toolbar
+def openFile():
+	pass
+
+def saveFile():
+	pass
+
+toolsbar = Menu(window)
+window.config(menu=toolsbar)
+
+filebar = Menu(toolsbar, tearoff=0)
+toolsbar.add_cascade(label="File",menu=filebar)
+
+filebar.add_command(label="Open", command=openFile)
+filebar.add_command(label="Save",command=saveFile)
 
 
 
