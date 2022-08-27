5de2c0d07da09d2349fb65c225b4c84b40de6fa3
Adithya Dsilva <adithyadsilva@icloud.com>
2022-08-27 19:42:41 +0530

    Remove useless file.

--- server/models/symblai.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-
-def cache_data(data_name: str, video_id: str, data: dict):
-    file_path = f"new_processed_test_data/{data_name}/{video_id}.json"
-    os.makedirs(file_path, exist_ok=True)
-
-    json.dump(data, open(file_path, "w+"))
