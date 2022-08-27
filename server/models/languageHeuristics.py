0c3f96d8f5e946b9c341c54301c0718c413f7189
Andy Yu <ayu303@gatech.edu>
2022-08-27 19:16:42 -0400

    Add More Language Heuristics

--- server/models/languageHeuristics.py
+++ server/models/languageHeuristics.py
@@ -1,14 +1,12 @@
 from readability import Readability
+from readability.text.syllables import count as syllableCount
 from readability.exceptions import ReadabilityException
+from pysyllables import get_syllable_count
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import string
-"""
-TODO: 
-readability test (Dale-Chall)
-topic frequency w/ symbl.ai
-"""
+import re
 
 def relativeDCReadability(text: str, totalReadability: float) -> float:
     """
@@ -31,7 +29,7 @@ def relativeDCReadability(text: str, totalReadability: float) -> float:
         return relativeDCReadability(text * textExtensionFactor, totalReadability)
     return textDC.score/totalReadability
 
-def lexicalDiversity(text)->float:
+def lexicalDiversity(text:str)->float:
     """
     Returns the fraction of words in a given text that are not stopwords.
     """
@@ -46,3 +44,56 @@ def lexicalDiversity(text)->float:
     textLength = len(textWords)
     textWords = [w for w in textWords if not w.lower() in stopWords]
     return len(textWords)/textLength
+
+def posFractions(text:str)->dict:
+    """
+    Given a string, returns the share of various parts of speech.
+    """
+    try:
+        nltk.data.find('taggers\\averaged_perceptron_tagger')
+    except LookupError:
+        nltk.download('averaged_perceptron_tagger')
+    try:
+        nltk.data.find('tokenizers/punkt')
+    except LookupError:
+        nltk.download('punkt')
+
+
+    tokens = word_tokenize(text)
+    punctSet = set(string.punctuation)
+    #strip punctuation
+    tokens = [w for w in tokens if not w in punctSet]
+    totalWords = len(tokens)
+    tagged_words = nltk.pos_tag(tokens)
+    tokenCounter = dict()
+    for token in tagged_words:
+        if token[1] in tokenCounter.keys():
+            tokenCounter[token[1]] += 1
+        else:
+            tokenCounter[token[1]] = 1
+    for pos in tokenCounter.keys():
+        tokenCounter[pos] = round(tokenCounter[pos]/totalWords, 3)
+    return tokenCounter
+
+def syllablesPerSecond(text:str, time: float)->float:
+    """
+    Given a string and the time it takes to speak said string (in seconds), 
+    return the speaking rate in terms of syllables per second.
+    text: the text to be parsed.
+    time: the time in seconds it took to speak
+    """
+    try:
+        nltk.data.find('tokenizers/punkt')
+    except LookupError:
+        nltk.download('punkt')
+    tokens = word_tokenize(text)
+    punctSet = set(string.punctuation)
+    #strip punctuation
+    tokens = [w for w in tokens if not w in punctSet]
+    totalSyllables = 0
+    for word in tokens:
+        if get_syllable_count(word) != None:
+            totalSyllables += get_syllable_count(word)
+        else:
+            totalSyllables += syllableCount(word)
+    return round(totalSyllables/time, 3)
